9f4a73879c2b645475b8a84f939767a9fe0c7a93
rsouza <rafael.souza.fijalkowski@denkwerk.com>
2020-12-17 02:14:27 +0100

    Improve sitemap.

--- src/sitemap/sitemap.ts
+++ src/sitemap/sitemap.ts
@@ -6,10 +6,16 @@ const dynamoDb = new DynamoDB.DocumentClient();
 exports.handler = (event, context, callback) => {
     const paramsGet = {
         TableName: process.env.DYNAMODB_TABLE,
+        IndexName: 'latest-items-index',
+        KeyConditionExpression: 'main = :main',
+        ExpressionAttributeValues: {
+            ':main': 0,
+        },
+        ScanIndexForward: false,
         Limit: 500,
     };
 
-    dynamoDb.scan(paramsGet, (err, data) => {
+    dynamoDb.query(paramsGet, (err, data) => {
         if (err) {
             Response.error(callback, err.code, err.message);
         } else {
@@ -19,6 +25,13 @@ exports.handler = (event, context, callback) => {
                 data.Items.forEach((item) => {
                     listUrl += 'https://newleaf.app/@' + item.id + '\r\n';
                 });
+                listUrl += 'https://newleaf.app\r\n';
+                listUrl += 'https://newleaf.app/faq\r\n';
+                listUrl += 'https://newleaf.app/donation\r\n';
+                listUrl += 'https://newleaf.app/contact\r\n';
+                listUrl += 'https://newleaf.app/privacy-policy\r\n';
+                listUrl += 'https://newleaf.app/terms-of-service\r\n';
+                listUrl += 'https://newleaf.app/@rafael-souza-fijalkowski\r\n';
                 Response.sendText(callback, 200, listUrl);
             } else {
                 Response.sendText(callback, 200, '');
